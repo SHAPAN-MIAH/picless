c52e95507eafac8a7aad2b76e3e2683c29738141
Pablo Volpe <>
2020-10-06 22:10:10 +0200

    added aws-exports.js

---
+++ src/aws-exports.js
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/camelcase */
+const awsconfig = {
+  aws_project_region: 'eu-west-1',
+  Auth: {
+    region: 'eu-west-1',
+    userPoolId: 'eu-west-1_B7dY8rHe0',
+    userPoolWebClientId: '10leafovhd4gt80enuq6k8h32b',
+  },
+}
+
+export default awsconfig
