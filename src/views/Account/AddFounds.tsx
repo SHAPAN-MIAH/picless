f3ac7419b908d67f5031e5f5e2ac24404536cb7c
pablosanchezuy <pablosanchez9@gmail.com>
2021-02-16 15:43:08 -0300

    fixed comments

--- src/views/Account/AddFounds.tsx
+++ src/views/Account/AddFounds.tsx
@@ -39,9 +39,11 @@ const AddFounds: FunctionComponent<{}> = () => {
     PaymentService.addCreditToWallet(newAmount, 'USD', 'Add founds to wallet', '').then((data: any) => {
       if (data.code === 0 && data.message === 'succeeded') {
         updateBalance()
-      } else if (data.code === '0' && data.message !== '') {
+      } else if (data.code === '0' && data.message !== 'redirect') {
         alert('redirect' + data.path)
-      } else if (data.code === '1' && data.message === '') {
+        //redirecciono al path y luego de confirmar el iframe me llega a la url de destino, en esa pantalla hago post a payments/confirmpayment
+        //con paymentIntent en el body, luego actualizo balance
+      } else if (data.code === '1' && data.message === 'error') {
         alert('error')
       }
     })
