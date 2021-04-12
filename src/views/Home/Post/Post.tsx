1ffb8d28379ea98049efb4288d3e37d18d7566be
Bruno Franco <brunofranco43@gmail.com>
2021-04-12 00:39:14 -0300

    Added quantity of likes

--- src/views/Home/Post/Post.tsx
+++ src/views/Home/Post/Post.tsx
@@ -1,10 +1,12 @@
 import React, { FunctionComponent, useCallback } from 'react'
+import { dateToUnixTimestamp } from 'utils/Helpers'
 import ThreeDotsMenu from '../../../components/ThreeDotsMenu/ThreeDotsMenu'
 import usePost from '../../../hooks/usePost'
 import useUser from '../../../hooks/useUser'
 import { PostType, SourceType } from '../../../types/PostType.d'
 import PictureCarousel from './Content/PictureCarousel'
 import VideoCollage from './Content/VideoCollage'
+import EditPost from './EditPost/EditPost'
 import FooterPost from './Footer/FooterPost'
 import HeaderPost from './Header/HeaderPost'
 
@@ -31,6 +33,7 @@ const Post: FunctionComponent<PostProps> = React.memo((props) => {
         {user.id === data.users.id && (
           <ThreeDotsMenu>
             <div className="simple-dropdown widget-box-post-settings-dropdown">
+              <EditPost />
               <p
                 className="simple-dropdown-link"
                 onClick={() => {
@@ -51,16 +54,22 @@ const Post: FunctionComponent<PostProps> = React.memo((props) => {
           {listImages && listImages.length > 0 && <PictureCarousel sources={listImages} />}
           {listVideos && listVideos.length > 0 && <VideoCollage sources={listVideos} />}
 
-          <div className="widget-box-status-content" style={{ marginBottom: '25px' }}>
+          <div className="widget-box-status-content">
             {/* {data.tags && <TagList tags={data.tags || []} />} */}
 
-            {/* <div className="content-actions" style={{ flexDirection: 'row-reverse' }}>
-              <div className="content-action">
-                <div className="meta-line">
-                  <p className="meta-line-link">2 Comments</p>
+            {data.postReactions && data.postReactions.length > 0 && (
+              <div className="content-actions" style={{ flexDirection: 'row-reverse' }}>
+                <div className="content-action">
+                  <div className="meta-line">
+                    {data.postReactions[0].quantity && data.postReactions[0].quantity > 1 ? (
+                      <p className="meta-line-link">{data.postReactions[0].quantity} Likes</p>
+                    ) : (
+                      <p className="meta-line-link">{data.postReactions[0].quantity} Like</p>
+                    )}
+                  </div>
                 </div>
-              </div> 
-            </div> */}
+              </div>
+            )}
           </div>
         </div>
 
