ebd8c0e29efe970dcb6ff11b30da59b4ee535ddc
Milton <milton@picless.com>
2021-05-03 10:54:48 +0200

    changes

--- src/hooks/useNotification.ts
+++ src/hooks/useNotification.ts
@@ -1,39 +1,3 @@
-<<<<<<< HEAD
-import { useContext, useEffect } from 'react'
-import toast from 'react-hot-toast'
-
-import useInterval from './commons/useInterval'
-
-import ApplicationContext from '../context/ApplicationContext'
-import { ServiceNotificationType } from '../types/NotificationType'
-import NotificationService from '../services/NotificationService'
-
-const useNotifications = () => {
-  const { notifications, setNotifications } = useContext(ApplicationContext.context)
-
-  const getNotifications = async (): Promise<void> => {
-    return NotificationService.getNotifications().then((p: ServiceNotificationType): void => {
-      if (p.code === '0') {
-        setNotifications(p.data)
-      } else {
-        toast.error('Error loading notifications')
-      }
-    })
-  }
-
-  useEffect(() => {
-    getNotifications()
-  }, [])
-
-  useInterval(() => {
-    getNotifications()
-  }, 300000)
-
-  return { getNotifications, notifications }
-}
-
-export default useNotifications
-=======
 import { useContext, useEffect } from 'react'
 import toast from 'react-hot-toast'
 
@@ -64,4 +28,3 @@ const useNotifications = () => {
 }
 
 export default useNotifications
->>>>>>> stage-5
--- src/views/Metrics/Charts/DailyIncome.tsx
+++ src/views/Metrics/Charts/DailyIncome.tsx
@@ -1,58 +1,38 @@
 import React, { FunctionComponent, useState } from 'react'
 import {
-  BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, AreaChart, Area
+  BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, LabelList
 } from 'recharts';
 
 
 const DailyIncome: FunctionComponent<{}> = () => {
 
   const data = [
-    {
-      name: 'Page A', uv: 4000, pv: 2400, amt: 2400,
-    },
-    {
-      name: 'Page B', uv: 3000, pv: 1398, amt: 2210,
-    },
-    {
-      name: 'Page C', uv: 2000, pv: 9800, amt: 2290,
-    },
-    {
-      name: 'Page D', uv: 2780, pv: 3908, amt: 2000,
-    },
-    {
-      name: 'Page E', uv: 1890, pv: 4800, amt: 2181,
-    },
-    {
-      name: 'Page F', uv: 2390, pv: 3800, amt: 2500,
-    },
-    {
-      name: 'Page G', uv: 3490, pv: 4300, amt: 2100,
-    },
+    { name: 'food', uv: -2000, pv: -2013, amt: -4500, bmk: -4301, time: 1, uvError: [100, 50], pvError: [110, 20] },
+    { name: 'cosmetic', uv: 3300, pv: 2000, amt: 6500, bmk: 2000, time: 2, uvError: 120, pvError: 50 },
+    { name: 'storage', uv: 3200, pv: 1398, amt: 5000, bmk: 3000, time: 3, uvError: [120, 80], pvError: [200, 100] },
+    { name: 'digital', uv: 2800, pv: 2800, amt: 4000, bmk: 1500, time: 4, uvError: 100, pvError: 30 },
   ];
 
   return (
     <>
-        <AreaChart
-          width={500}
-          height={300}
-          data={data}
-          margin={{
-            top: 5, right: 30, left: 20, bottom: 5,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Area
-                type="monotone"
-                dataKey="Users.count"
-                name="Users"
-                stroke="rgb(106, 110, 229)"
-                fill="rgba(106, 110, 229, .16)"
-              />
-        </AreaChart>
+      <p>Stack BarChart</p>
+        <div className="bar-chart-wrapper">
+          <BarChart width={400} height={400} data={data}>
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <CartesianGrid vertical={false} />
+            <Bar stackId="0" dataKey="uv" fill="#ff7300">
+              <LabelList />
+            </Bar>
+            <Bar stackId="0" dataKey="pv" fill="#387908" />
+            <Bar dataKey="amt" fill="#387908">
+              <LabelList />
+            </Bar>
+            <Legend layout="vertical" />
+          </BarChart>
+        </div>
+
     </>
   )
 }
