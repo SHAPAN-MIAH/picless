c64c2f51b058ab0f7ec905316eaa60f44a616b69
Milton <milton@picless.com>
2021-05-07 08:34:04 +0200

    pruebs swips photo

---
+++ src/views/TestView/Img.tsx
@@ -0,0 +1,22 @@
+import React, { FunctionComponent } from 'react'
+import styles from './PictureCarousel.module.css'
+
+type PictureCarouselProps = {
+
+  amount: number
+  blocked: boolean
+}
+
+
+const PictureCarousel: FunctionComponent<PictureCarouselProps> = React.memo((props) => {
+
+  
+  
+
+  return (
+    <>
+    </>
+  )
+})
+
+export default PictureCarousel
