264a19693f068393f9c1f65af3e15f50dcdcdd7e
Milton <mayloevan@gmail.com>
2021-05-08 10:49:55 +0200

    cambio de color del floaty bar y los iconos

--- src/views/LayoutMain/FloatyBar/FloatyBar.tsx
+++ src/views/LayoutMain/FloatyBar/FloatyBar.tsx
@@ -19,7 +19,7 @@ const FloatyBar: FunctionComponent<{}> = () => {
 
   return (
     <>
-      <aside className="floaty-bar" style={{ zIndex: 999999 }}>
+      <aside className="floaty-bar" style={{ zIndex: 999999, backgroundColor: '#FFF', borderTop: '1px solid #eaeaf5'}}>
         <div className="bar-actions"> </div>
 
         <div className="bar-actions">
@@ -27,7 +27,7 @@ const FloatyBar: FunctionComponent<{}> = () => {
             <Link className="action-list-item" to="/user/home">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                 <path
-                  fill="#8b88ff"
+                  fill="#000000"
                   d="M20 7.093v-5.093h-3v2.093l3 3zm4 5.907l-12-12-12 12h3v10h7v-5h4v5h7v-10h3zm-5 8h-3v-5h-8v5h-3v-10.26l7-6.912 7 6.99v10.182z"
                 />
               </svg>
@@ -35,20 +35,20 @@ const FloatyBar: FunctionComponent<{}> = () => {
 
             <Link className="action-list-item unread" to="/user/notification">
               <svg className="action-list-item-icon icon-notification">
-                <use xlinkHref="#svg-notification" />
+                <use xlinkHref="#svg-notification" style={{ fill: "#000000"}}/>
               </svg>
             </Link>
 
             {user.verifiedAccount ? (
               <Link className="action-list-item" to="/user/create-post">
                 <svg className="action-list-item-icon icon-plus">
-                  <use xlinkHref="#svg-plus" />
+                  <use xlinkHref="#svg-plus" style={{ fill: "#000000"}}/>
                 </svg>
               </Link>
             ) : (
               <Link className="action-list-item" to="/user/discover">
                 <svg className="action-list-item-icon icon-magnifying-glass">
-                  <use xlinkHref="#svg-magnifying-glass" />
+                  <use xlinkHref="#svg-magnifying-glass" style={{ fill: "#000000"}}/>
                 </svg>
               </Link>
             )}
@@ -56,7 +56,7 @@ const FloatyBar: FunctionComponent<{}> = () => {
             <Link className="action-item-wrap" to="/user/messages">
               <div className="action-item">
                 <svg className="action-item-icon icon-messages">
-                  <use xlinkHref="#svg-messages" />
+                  <use xlinkHref="#svg-messages" style={{ fill: "#000000"}}/>
                 </svg>
               </div>
             </Link>
@@ -64,7 +64,7 @@ const FloatyBar: FunctionComponent<{}> = () => {
             <a className="action-list-item" href="" onClick={handleMenu}>
               <div className="menu-item-link-icon icon-members">
                 <svg className="icon-members">
-                  <use xlinkHref="#svg-members" />
+                  <use xlinkHref="#svg-members" style={{ fill: "#000000"}}/>
                 </svg>
                 <div className="burger-icon-bar" />
 
