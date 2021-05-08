305fae9c54aab1030ddd77c6b5983d7d4aa1280c
Milton <mayloevan@gmail.com>
2021-05-08 09:27:23 +0200

    conflicot con el archivo de iconos

--- src/utils/Icons.ts
+++ src/utils/Icons.ts
@@ -1,65 +1,3 @@
-<<<<<<< HEAD
-import { library } from '@fortawesome/fontawesome-svg-core'
-import {
-  faCheck,
-  faPlus,
-  faTimes,
-  faDollarSign,
-  faShareAlt,
-  faComments,
-  faLock,
-  faHeart,
-  faRedo,
-  faHome,
-  faExclamationTriangle,
-  faStar,
-  faCreditCard,
-  faSatelliteDish,
-  faExpand,
-  faSyncAlt,
-  faStopCircle,
-  faChevronCircleLeft,
-  faChevronCircleRight,
-  faBars,
-  faMicrophone,
-  faMicrophoneSlash,
-  faPlayCircle,
-  faSignOutAlt,
-  faArrowLeft,
-} from '@fortawesome/free-solid-svg-icons'
-import { faCcVisa, faCcMastercard, faCcAmex, faCcDinersClub, faCcJcb } from '@fortawesome/free-brands-svg-icons'
-
-// COMMON ICONS
-library.add(
-  faPlus,
-  faTimes,
-  faCheck,
-  faDollarSign,
-  faShareAlt,
-  faComments,
-  faLock,
-  faHeart,
-  faRedo,
-  faHome,
-  faExclamationTriangle,
-  faSatelliteDish,
-  faExpand,
-  faSyncAlt,
-  faStopCircle,
-  faChevronCircleLeft,
-  faChevronCircleRight,
-  faBars,
-  faMicrophone,
-  faMicrophoneSlash,
-  faPlayCircle,
-  faSignOutAlt,
-  faPlus,
-  faArrowLeft
-)
-
-// BRAND ICONS
-library.add(faCcVisa, faCcMastercard, faCcAmex, faCcDinersClub, faCcJcb, faStar, faCreditCard)
-=======
 import { library } from '@fortawesome/fontawesome-svg-core'
 import {
   faCheck,
@@ -121,4 +59,3 @@ library.add(
 
 // BRAND ICONS
 library.add(faCcVisa, faCcMastercard, faCcAmex, faCcDinersClub, faCcJcb, faStar, faCreditCard)
->>>>>>> stage-5
