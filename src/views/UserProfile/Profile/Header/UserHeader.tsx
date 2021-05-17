0c13953649dc5c0505a5c8b276cc2a1867c8ce1a
Milton <milton@picless.com>
2021-05-17 10:03:51 +0200

    quitar bordes en el grid de fotos y centrar la bandera

--- src/views/UserProfile/Profile/Header/UserHeader.tsx
+++ src/views/UserProfile/Profile/Header/UserHeader.tsx
@@ -142,7 +142,7 @@ const UserHeader: FunctionComponent<UserHeaderProps> = (props) => {
                 <p className="user-stat-text">Videos</p>
               </div>
 
-              <div className="user-stat big flag" style={{ display: 'block', padding: '0 8px' }}>
+              <div className="user-stat big flag">
                 <CountryFlag className="user-stat-image" code={provider.countryCode || ''} alt={countryName} />
 
                 <p className="user-stat-text">{countryName}</p>
