f707ede601cb6d201df11f8188708721e3dc0a6a
Michael Molina <michaelmolina679@gmail.com>
2021-06-17 13:41:56 -0500

    Adding new functionality to the share button

--- src/components/ShareComponent/ShareComponent.tsx
+++ src/components/ShareComponent/ShareComponent.tsx
@@ -38,60 +38,69 @@ const ShareComponent: FunctionComponent<ShareProps> = React.memo(() => {
             contentStyle={{ padding: '0px', border: 'none', width: '140px', borderRadius: '12px', marginLeft: '100px' }}
             arrow={false}
           >
-            <Popup
-              modal
-              contentStyle={{ width: 'auto', borderRadius: '5px', minWidth: '' }}
-              position="center center"
-              trigger={
-                <span className="post-option">
-                  <p>Share Profile</p>
-                </span>
-              }
-            >
-              {/Android|webOS|iPhone|iPad|Mac|Macintosh|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ? (
-                <button
-                  onClick={() => {
-                    navigator.share({
-                      title: 'Hi my friend, You want to Join US!',
-                      text: 'Join us in PicLess',
-                      url: `michael.lup20.uk/u/${provider.userName}`,
-                    })
-                  }}
-                >
-                  Share Profile
-                </button>
-              ) : (
-                <div className={styles.mainPopup}>
-                  <div className={styles.closePopup}>
-                    <FontAwesomeIcon icon="times" color="white" size="1x" />
-                  </div>
-                  <form>
-                    <FormRowItem>
-                      <div className={styles.userInfoContainer}>
-                        <div className={styles.userInfoName}>
-                          <CopyToClipboardComponent userProfile={provider.userName} />
-                          {/* <p className="user-status-title">
-                            <span className="bold">Share to</span>
-                          </p>
-                          <p className="user-status-text small">
-                            <a href={`/u/hello`}>https://michael.lup20.uk/u/{provider.userName}</a>
-                          </p> */}
-                        </div>
-                      </div>
-                    </FormRowItem>
-                    <FormRowItem>
-                      <h4>Share To</h4>
-                      <SocialLinkList userProfile={provider.userName} />
-                    </FormRowItem>
-                  </form>
-                </div>
-              )}
-            </Popup>
+            <SharePopup username={provider.userName} />
           </Popup>
         </div>
       </div>
     </>
   )
 })
+
+const SharePopup = (username: any) => {
+  if (/Android|webOS|iPhone|iPad|Mac|Macintosh|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    return (
+      <button
+        className="post-option"
+        onClick={() => {
+          navigator.share({
+            title: 'Hi my friend, You want to Join US!',
+            text: 'Join us in PicLess',
+            url: `michael.lup20.uk/u/${username}`,
+          })
+        }}
+      >
+        Share Profile
+      </button>
+    )
+  } else {
+    return (
+      <Popup
+        modal
+        contentStyle={{ width: 'auto', borderRadius: '5px', minWidth: '' }}
+        position="center center"
+        trigger={
+          <span className="post-option">
+            <p>Share Profile</p>
+          </span>
+        }
+      >
+        <div className={styles.mainPopup}>
+          <div className={styles.closePopup}>
+            <FontAwesomeIcon icon="times" color="white" size="1x" />
+          </div>
+          <form>
+            <FormRowItem>
+              <div className={styles.userInfoContainer}>
+                <div className={styles.userInfoName}>
+                  <CopyToClipboardComponent userProfile={username} />
+                  {/* <p className="user-status-title">
+                <span className="bold">Share to</span>
+              </p>
+              <p className="user-status-text small">
+                <a href={`/u/hello`}>https://michael.lup20.uk/u/{provider.userName}</a>
+              </p> */}
+                </div>
+              </div>
+            </FormRowItem>
+            <FormRowItem>
+              <h4>Share To</h4>
+              <SocialLinkList userProfile={username} />
+            </FormRowItem>
+          </form>
+        </div>
+      </Popup>
+    )
+  }
+}
 
 export default ShareComponent
